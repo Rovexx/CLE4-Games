f0271b87f83a0c8b5ce3896574ad16a8bdf0974e
Branko <branko@pixeldeluxe.nl>
2018-05-16 15:25:11 +0200

    powerups work in progress

---
+++ js/powerups.js
@@ -0,0 +1,26 @@
+class Powerup {
+	constructor(init, xPos, yPos, aantal) {
+		aantal = aantal - 1;
+		this.game = init;
+
+        this.sprite = init.physics.add.group({
+            key: 'powerup_icon',
+            repeat: aantal,
+            setXY: { x: 80, y: 30, stepX: 200 , stepY: 160 }
+        });
+
+        this.sprite.children.iterate(function (child) {
+			child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+            child.setScale(0.2);
+            child.body.allowGravity = false;
+        });
+	}
+
+	update() {
+
+	}
+
+	collectPowerup() {
+		console.log('powerup collected!');
+	}
+}
