aa1cc27ab41f9cae5813093983b8168e6126e52b
Branko <branko@pixeldeluxe.nl>
2018-05-16 15:28:34 +0200

    merge conflicts resolven

--- js/main.js
+++ js/main.js
@@ -19,6 +19,7 @@ var config = {
 var game = new Phaser.Game(config);
 
 var ground;
+var powerup;
 var player;
 var gameOver = false;
 
@@ -28,6 +29,7 @@ function preload() {
     this.load.image("background_1", "assets/Background_1.png");
     this.load.image("ai", "assets/ai.png");
     this.load.image("fish_tmp", "assets/fish_tmp.png");
+    this.load.image("powerup_icon", "assets/powerup.png");
 }
 
 function create() {
@@ -37,10 +39,11 @@ function create() {
 
     // Create the player:
     player = new Player(this, 200, 100);
+    powerup = new Powerup(this, 300, 500, 4);
+
+    this.physics.add.overlap(player, powerup, collectPowerup, null, this);
   
     AIs.push(new Ai(this, 500, 400))
-
-    
 }
 
 function update() {
@@ -48,11 +51,13 @@ function update() {
         return;
     }
 
-
     for (var ai of AIs) {
         ai.update()
     }
 
     player.update(this);
+}
 
+function collectPowerup() {
+    console.log('Powerup collected');
 }
